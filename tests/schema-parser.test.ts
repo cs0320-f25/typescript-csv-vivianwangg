c4c24e3f6576910f55ffe186ec921e57598c905d
Vivian Wang <vivian_z_wang@brown.edu>
2025-09-12 17:09:19 -0400

    testing schema parser: correct schema

--- tests/schema-parser.test.ts
+++ tests/schema-parser.test.ts
@@ -1,3 +1,18 @@
 import { parseCSV } from "../src/basic-parser";
 import * as path from "path";
+import { z } from "zod";
 
+const TEST_CSV_PATH = path.join(__dirname, "../data/peopleschema.csv");
+
+test("parseCSV works with input schema", async () => {
+	const schema = z.tuple([z.string(), z.string()]);
+
+	const result = await parseCSV(TEST_CSV_PATH, schema);
+
+	expect(result).toEqual([
+		["Alice", "London"],
+		["Bob", "Boston"],
+		["Charlie", "Lexington"],
+		["Nim", "Tampa"],
+	]);
+});
