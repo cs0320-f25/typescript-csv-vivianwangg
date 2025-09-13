b70999722759c3267de12fb5ac0941312fd81158
Vivian Wang <vivian_z_wang@brown.edu>
2025-09-12 20:13:37 -0400

    tests with schema input added

--- tests/schema-parser.test.ts
+++ tests/schema-parser.test.ts
@@ -4,32 +4,42 @@ import { z } from "zod";
 
 const CORRECT_CSV_PATH = path.join(__dirname, "../data/schemaCorrect.csv");
 const INCORRECT_CSV_PATH = path.join(__dirname, "../data/schemaIncorrect.csv");
+const NOSCHEMA_CSV_PATH = path.join(__dirname, "../data/noschema.csv");
 
-test("parseCSV works with input schema and correct csv", async () => {
-	const schema = z.tuple([z.string(), z.string()]);
+test("parseCSV parses CSV with all successful rows", async () => {
+    const schema = z.tuple([z.string(), z.coerce.number()]);
 
-	const result = await parseCSV(CORRECT_CSV_PATH, schema);
+    const result = await parseCSV(CORRECT_CSV_PATH, schema);
 
-	expect(result).toEqual([
-		["Alice", "London"],
-		["Bob", "Boston"],
-		["Charlie", "Lexington"],
-		["Nim", "Tampa"],
-	]);
+    result.forEach((row) => {
+      if ("success" in row) {
+        expect(row.success).toBe(true);
+      } else {
+        throw new Error("Expected ZodSafeParseResult");
+      }
+    });
+}); 
+
+test("returns ZodSafeParseResult[] when schema is provided and some rows are invalid", async () => {
+  const schema = z.tuple([z.string(), z.coerce.number()]);
+
+  const result = await parseCSV(INCORRECT_CSV_PATH, schema);
+
+  if ("success" in result[0] && "success" in result[1] && "success" in result[2]) {
+    expect(result[0].success).toBe(true); 
+    expect(result[1].success).toBe(false);  
+    expect(result[2].success).toBe(true);   
+  } else {
+    throw new Error("Expected ZodSafeParseResult[], got string[][]");
+  }
 });
 
-test("parseCSV returns error with input schema and incorrect csv", async () => {
-	const schema = z.tuple([z.string(), z.string()]);
-	const result = await parseCSV(CORRECT_CSV_PATH, schema);
-	expect(result).toEqual(z.ZodError)
-});
+test("returns string[][] when no schema is provided", async () => {
+    const result = await parseCSV(NOSCHEMA_CSV_PATH);
 
-test("parseCSV returns string[][] when input schema undefined", async () => {
-	const result = await parseCSV(INCORRECT_CSV_PATH);
-	expect(result).toEqual([
-		["Alice", "London"],
-		["Bob", "Boston"],
-		["Charlie", "Lexington"],
-		["Nim", "Tampa"],
-	]);
+    expect(result).toEqual([
+      ["X", "Y", "Z"],
+      ["1", "2", "3"],
+      ["4", "5", "6"],
+    ]);
 });
